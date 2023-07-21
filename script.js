6b37413cd11c2c6b6b0bad56c0d9b611224965fc
Uday Bhatiya <139557497+uday-bhatiya@users.noreply.github.com>
2023-07-21 21:57:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const userText = document.getElementById('inputText');
+const speechBtn = document.getElementById('speech');
+
+speechBtn.addEventListener('click', function() {
+    speechText = userText.value;
+
+    let speechData = new SpeechSynthesisUtterance();
+
+    speechData.text = speechText;
+
+    speechSynthesis.speak(speechData);
+
+})
